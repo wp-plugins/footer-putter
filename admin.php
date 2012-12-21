c3eaee1dc4440e474baea11c9fb9ede8f474d04a
powerblogservice <powerblogservice@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-21 04:33:59 +0000

    Footer Putter 1.0
    git-svn-id: https://plugins.svn.wordpress.org/footer-putter/trunk@642736 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin.php
@@ -0,0 +1,99 @@
+<?php
+class FooterPutterAdmin {
+    const CREDITS = 'FooterCreditsAdmin';
+    private $pagehook;
+
+	function __construct() {
+		add_filter('screen_layout_columns', array(&$this, 'screen_layout_columns'), 10, 2);
+		add_filter('plugin_action_links',array(&$this, 'plugin_action_links'), 10, 2 );
+		add_action('admin_menu', array(&$this,'admin_menu'));
+		call_user_func (array(self::CREDITS,'init'),FOOTER_PUTTER_PLUGIN_NAME,FOOTER_PUTTER_VERSION);
+	}
+
+	function screen_layout_columns($columns, $screen) {
+		if (!defined( 'WP_NETWORK_ADMIN' ) && !defined( 'WP_USER_ADMIN' )) {
+			if ($screen == $this->pagehook) {
+				$columns[$this->pagehook] = 2;
+			}
+		}
+		return $columns;
+	}
+
+	function plugin_action_links( $links, $file ) {
+		if ( is_array($links) && (FOOTER_PUTTER_PATH == $file )) {
+			$settings_link = '<a href="' . admin_url( 'admin.php?page='.FOOTER_PUTTER_PLUGIN_NAME) . '">Settings</a>';
+			array_unshift( $links, $settings_link );
+		}
+		return $links;
+	}
+
+	function admin_menu() {
+		$this->pagehook = add_menu_page(FOOTER_PUTTER_FRIENDLY_NAME, FOOTER_PUTTER_FRIENDLY_NAME, 'manage_options', 
+			FOOTER_PUTTER_PLUGIN_NAME, array(&$this,'resources_panel'),plugins_url('/menu-icon.png',__FILE__) );
+	}
+	
+	function resources_panel() {
+    	$footer_url = call_user_func(array(self::CREDITS, 'get_url')); 
+    	$home_url = FOOTER_PUTTER_HOME_URL;
+    	$version = FOOTER_PUTTER_VERSION;
+    	$plugin = FOOTER_PUTTER_FRIENDLY_NAME;
+		$screenshot = plugins_url('screenshot1.jpg',__FILE__);    	
+		$logo = plugins_url('logo.png',__FILE__);    	
+    	print <<< ADMIN_PANEL
+<div class="wrap">
+<h2>{$plugin} {$version} Overview</h2>
+<img class="alignright" src="{$logo}" alt="Footer Putter Plugin" />
+
+<p>{$plugin} allows you to put a footer to your site that adds credibility to your site, with BOTH visitors and search engines.</p>
+<p>Google is looking for some indicators that the site is about a real business.</p>
+<ol>
+<li>The name of the business or site owner</li>
+<li>A copyright notice that is up to date</li>
+<li>A telephone number</li>
+<li>A postal address</li>
+<li>Links to Privacy Policy and Terms of Use pages</p>
+</ol>
+
+<p>Human visitors may pay some credence to this information but will likely be more motivated by trade marks, trust marks and service marks.</p>
+
+<h2>{$plugin} Widgets</h2>
+
+The plugins define two widgets: 
+<ol>
+<li>a <b>Footer Copyright Widget</b> that places a line at the foot of your site containing as many of the items listed above that you want to disclose.</li>
+<li>a <b>Trademarks Widget</b> that displays a line of trademarks that you have previously set up as "Links".
+</ol>
+<p>All footer links are rel=nofollow in line with best SEO recommendations.</p>
+<p>Typically you will drag both widgets into the Custom Footer Widget Area.</p>
+<p>Your footer will look something like this:</p>
+<img src="{$screenshot}" alt="Screenshot of Footer Credits Widget Area" />
+
+<h2>Instructions For WPWhoosh Users</h2>
+<p>If you have <a href="http://www.wpwhoosh.com/">whooshed</a> this site then all you need to do is replace the sample trademarks with the real trademarks.
+
+<h2>Instructions For Other Users</h2>
+<ol>
+<li>Create a <i>Privacy Policy</i> page with no sidebar and set robots meta as noindex, noarchive.</li>
+<li>Create a <i>Terms of Use</i> page with no sidebar and set robots meta as noindex, noarchive.</li>
+<li>Create a page with a contact form.</li>
+<li>Create a WordPress menu <i>Footer Menu</i> with the above 3 pages.</li>
+<li>Go to <a href="{$footer_url}">Footer Credits</a> and update the Site Owner details and set the Footer Hook according to your choice of WordPress theme.</li>
+<li>Drag a <i>Footer Copyright Widget</i> into the <i>Custom Footer Widget Area</i> and select the <i>Footer Menu</i> and optional text if you want to have a "Return To Top" link</li>
+<li>Add a link for each of your trademarks and put each in a <i>Trademarks</i> link category - you can call this link category as you like</li>
+<li>Drag a Trademarks widget into the Custom Footer Widget and choose your <i>Trademarks</i> link category</li>
+</ol>
+
+<h3>Footer Hook</h3>
+
+<p>The footer hook is only required if your theme does not already have a footer widget area into which you can drag the two widgets.</p>
+
+<p>For some themes, the footer hook is left blank, for others use a WordPress hook such as <i>get_footer</i> or <i>wp_footer</i>, 
+or use a theme-specific hook such as <i>twentyten_credits</i>, <i>twentyeleven_credits</i>, <i>genesis_footer</i>, <i>pagelines_leaf</i>, etc</p>
+
+<h3>Getting Help</h3>
+<p>Check out the <a href="{$home_url}">Footer Putter Plugin page</a> for more information about the plugin.</p> 
+ADMIN_PANEL;
+	}
+}
+$footer_putter_admin = new FooterPutterAdmin();
+?>
---
+++ footer-credits.php
@@ -0,0 +1,419 @@
+<?php
+class FooterCredits {
+    const CLASSNAME = 'FooterCredits'; //name of class - must be same as line above - hard code for performance
+    const DOMAIN = 'FooterCredits'; //text domain for translation
+    const CODE = 'footer-credits'; //shortcode
+	const OPTIONS_NAME = 'footer_credits_options'; 
+	const SIDEBAR_ID = 'last-footer';
+    private static $version;
+	protected static $options  = array();
+	protected static $defaults  = array(
+		'terms' => array(
+			'site' => '',
+			'owner' => '',
+			'copyright' => '',
+			'copyright_start_year' => '',
+			'country' => '',
+			'courts' => '',
+			'email' => '',
+			'telephone' => '',
+			'address' => '',
+			'updated' => ''),
+		'nav_menu' => 0,
+		'separator' => '&nbsp;&middot;&nbsp;',
+		'center' => true,
+		'show_copyright' => true,
+		'show_telephone' => true,
+		'show_address' => true,
+		'show_return' => true,
+		'return_text' => 'Return To Top',
+		'return_href' => '#header',
+		'return_class' => '',
+		'footer_class' => '',			
+		'footer_hook' => ''
+	);
+
+    private static function get_version(){
+		return self::$version;
+	}
+	static function init($version) {
+		self::$version = $version;
+		add_action('widgets_init',array(self::CLASSNAME,'register'),20);		
+		add_filter( 'wp_nav_menu_items', array(self::CLASSNAME, 'fix_home_link'), 10, 2 );
+		if (!is_admin()) add_action('wp',array(self::CLASSNAME,'prepare'));	
+	}
+
+	static function register() {
+		self::register_sidebars();
+		self::register_widgets();
+	}
+
+	static function prepare() {
+		if (!is_admin()) {
+			add_shortcode(self::CODE.'-copyright', array(self::CLASSNAME, 'copyright_owner' ) );
+			add_shortcode(self::CODE.'-menu', array(self::CLASSNAME, 'footer_menu' ) );
+			add_shortcode(self::CODE, array(self::CLASSNAME, 'footer' ) );
+
+			add_filter('widget_text', 'do_shortcode', 11);		
+
+			add_action('wp_enqueue_scripts',array(self::CLASSNAME, 'add_styles' ));
+			
+			//insert custom footer at specified hook
+			if ($footer_hook = self::get_option('footer_hook'))  add_action( $footer_hook, array(self::CLASSNAME, 'custom_footer')); 
+	
+			if (is_page('terms') || is_page('privacy'))
+				add_filter('the_content', array(self::CLASSNAME, 'terms_filter') );	
+		}
+	}
+
+    static function register_sidebars() {
+    	if (self::get_option('footer_hook'))
+			register_sidebar( array(
+				'id' => self::SIDEBAR_ID,
+				'name'	=> __( 'Custom Footer Widget Area', self::CLASSNAME ),
+				'description' => __( 'Custom footer section for copyright, trademarks, etc', self::CLASSNAME),
+				'before_widget' => '<div id="%1$s" class="widget %2$s"><div class="widget-wrap">',
+				'after_widget'  => '</div></div>'	
+			) );
+    }
+	
+	static function register_widgets() {
+		register_widget( 'Footer_Putter_Copyright_Widget' );
+		register_widget( 'Footer_Putter_TradeMark_Widget' );
+	}	
+	
+	
+	static function add_styles() {
+		wp_enqueue_style(self::CLASSNAME, plugins_url('style.css',__FILE__), array(), self::get_version());
+    }
+
+	static function fix_home_link( $content, $args) {
+		if (strpos($content, '<a>Home</a>') !== FALSE) {
+			$class =  is_front_page()? ' class="current_page_item"' : '';
+			$content = str_replace ('<a>Home</a>',sprintf('<a href="%1$s"%2$s>Home</a>',home_url(),$class),$content); 
+		} 
+		return $content;
+	}
+
+	static function sanitize_terms($new_terms) {
+		$new_terms = wp_parse_args($new_terms, self::$defaults['terms']); //ensure terms are complete		
+		$new_terms['site'] = self::get_default_site();
+		$new_terms['copyright'] = self::get_copyright($new_terms); //generate copyright
+		return $new_terms;
+	}
+
+	static function save($new_options) {
+		$options = self::get_options(false);
+		$new_options = wp_parse_args( $new_options, $options);
+		$new_options['terms'] = self::sanitize_terms($new_options['terms']);
+		$updated = update_option(self::OPTIONS_NAME,$new_options);
+		if ($updated) self::get_options(false);
+		return $updated;
+	}	
+
+	static function get_options ($cache = true) {
+	   if ($cache && (count(self::$options) > 0)) return self::$options;
+	
+	   $the_options = array();
+	   $the_options = get_option(self::OPTIONS_NAME);
+	   if (empty($the_options)) {
+	      self::$options = self::$defaults;
+	   } else {
+			self::$options = shortcode_atts( self::$defaults, $the_options);
+	   }
+	   return self::$options;
+	}
+	
+	static function get_option($option_name) {
+    	$options = self::get_options();
+    	if ($option_name && $options && array_key_exists($option_name,$options))
+        	return $options[$option_name];
+    	else
+        	return false;    		
+    }
+    
+ 	static function get_terms() {
+    	return self::get_option('terms');
+    }   
+	
+	static function get_term($term_name) {
+    	$options = self::get_options();
+    	$terms = is_array($options) && array_key_exists('terms',$options) ? $options['terms'] : false;
+    	if ($term_name && $terms && array_key_exists($term_name,$terms) && $terms[$term_name])
+        	return $terms[$term_name];
+    	else
+        	return self::get_default_term($term_name);    		
+    }	
+	
+    static function get_default_term($key) {
+		$default='';
+    	switch ($key) {
+   			case 'owner' : $default = self::get_term('site'); break;
+   			case 'copyright' : $default = self::get_copyright(self::get_term('copyright_start_year')); break;
+   			case 'copyright_start_year': $default = date('Y'); break;
+   			case 'country' : $default = 'The United States'; break;
+   			case 'courts' : $default = ucwords(sprintf('the courts of %1$s',self::get_option('country'))); break;
+   			case 'email' : $default = 'privacy@'.strtolower(self::get_term('site')); break;
+   			case 'site' : $default = self::get_default_site(); break;
+   			case 'updated' : $default = date('d M Y'); break;
+ 			default: $default='';  //default is blank for others
+   		}
+   		return $default;
+    }
+	
+	static function get_default_site() { 
+		$domain = strtolower(parse_url(site_url(),PHP_URL_HOST));
+		$p = strpos($domain,'www.') ;
+		if (($p !== FALSE) && ($p == 0)) $domain = substr($domain,4);
+		return ucwords($domain); 
+	}
+	
+	static function get_copyright($startyear){
+  		$thisyear = date("Y");
+		if(empty( $startyear)) $startyear = $thisyear;
+  		return sprintf('Copyright &copy; %1$s%2$s', $startyear, $thisyear == $startyear ? '' : ("-".$thisyear));
+	}
+
+	static function copyright_owner($attr){
+		$defaults['owner'] = self::get_term('owner');
+		$defaults['copyright_start_year'] = self::get_term('copyright_start_year');		
+  		$params = shortcode_atts( $defaults, $attr ); //apply plugin defaults  		
+  		return sprintf('<span class="copyright">%1$s %2$s</span>', 
+  			self::get_copyright($params['copyright_start_year']), $params['owner']);
+	}	
+	
+    static function format_address ($address, $separator) {
+		$s='';
+		$addlines = explode(',', trim($address));
+		foreach ($addlines as $a) {
+			$a = trim($a);
+			if (!empty($a)) $s .= $a . $separator;
+		}
+		return $s;
+    }	
+	
+	static function footer_menu($menu) {
+        return self::filter_links(wp_nav_menu(array('menu' => $menu, 'echo' => false, 'container' => false)));
+	}
+
+	static function return_to_top( $text, $href, $class) {
+		if ('#'==$href) $href = self::$defaults['return_href'];
+		return sprintf( '<div class="%1$s"><a href="%2$s" rel="nofollow">%3$s</a></div>', trim($class), esc_url( $href), $text);
+	}
+
+
+	static function footer($atts = array()) {
+  		$params = shortcode_atts( self::get_options(), $atts ); //apply plugin defaults   
+		if ($params['center']) {
+			$line_separator = $params['separator'];
+			$params['return_class'] .= ' return-center';
+			$params['footer_class'] .= ' footer-center';
+			$clear = '';
+		} else {
+			$line_separator = '<br/>';
+			$params['return_class'] .= ' return-left';
+			$params['footer_class'] .= ' footer-right';
+			$clear = '<div class="clear"></div>';
+		}	
+		$copyright = self::copyright_owner(self::get_terms());
+		$tel = self::get_term('telephone');			
+		$address = self::get_term('address');
+		return (empty($params['show_return']) ? '' :
+			self::return_to_top($params['return_text'], $params['return_href'], $params['return_class'])) . 
+			sprintf('<div id="%1$s" class="%2$s">%3$s%4$s%5$s%6$s</div>%7$s<!-- end #%1$s -->', 
+				self::CODE,
+				$params['footer_class'], 	
+				(empty($params['nav_menu']) ? '' : self::footer_menu($params['nav_menu'])), 
+				(empty($params['show_copyright']) ? '' : sprintf('%1$s%2$s', $line_separator, $copyright)),
+				((empty($telephone) || empty($params['show_telephone'])) ? '' : sprintf('%1$s<span class="telephone">%2$s</span>', $line_separator, $telephone) ),
+				((empty($address) || empty($params['show_address'])) ? '' : sprintf('%1$s<span class="address">%2$s%3$s</span>', $line_separator, self::format_address($address, $params['separator']), self::get_term('country')) ),
+				$clear				
+			);				
+	}
+
+	static function terms_filter($content) {
+		if ($terms = self::get_terms()) {
+			$from = array();
+			$to = array();
+			foreach ($terms as $term => $value) {
+				$from[] = '%%'.$term.'%%';
+				$to[] = $value;
+			}
+			return str_replace($from,$to,$content);
+		} 
+		return $content;
+	}
+
+	static function custom_footer() {
+		if ( is_active_sidebar( self::SIDEBAR_ID) ) {
+			echo '<div class="custom-footer">';
+			dynamic_sidebar( self::SIDEBAR_ID );
+			echo '</div><!-- end .custom-footer -->';
+		}
+	}
+         
+    static function add_footer_filter() {
+ 		add_filter('wp_list_bookmarks', array(self::CLASSNAME,'filter_links'),20); //nofollow links in custom footer widgets
+    }    
+         
+    static function filter_links( $content) {
+		return preg_replace_callback( '/<a([^>]*)>(.*?)<\/a[^>]*>/is', array( self::CLASSNAME, 'nofollow_link' ), $content );
+    }		
+
+    static function nofollow_link($matches) { //make link nofollow
+		$attrs = shortcode_parse_atts( stripslashes ($matches[ 1 ]) );
+		if (isset($attrs['rel'])) return $matches[ 0 ];  //skip if already has a rel attribute
+		$atts='';
+		foreach ( $attrs AS $key => $value ) $atts .= sprintf('%1$s="%2$s" ', $key, $value);
+		$atts = substr( $atts, 0, -1 );
+		return sprintf('<a rel="nofollow" %1$s>%2$s</a>', $atts, $matches[ 2 ]);
+	}
+}
+
+class Footer_Putter_Copyright_Widget extends WP_Widget {
+
+	const DOMAIN = 'FooterCredits';
+
+	function __construct() {
+		$widget_ops = array( 'description' => __( "A widget displaying menu links, copyright and company details" ) );
+		parent::__construct('footer_copyright', __('Footer Copyright Widget'), $widget_ops);
+	}
+	
+	function widget( $args, $instance ) {
+		extract( $args );		
+		$footer_args=array();
+		echo $before_widget;
+		echo call_user_func(array(FOOTER_CREDITS,'footer'),$instance);
+		echo $after_widget;
+	}
+
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		$instance['nav_menu'] = !empty($new_instance['nav_menu']) ? $new_instance['nav_menu'] : 0;
+		$instance['show_copyright'] = !empty($new_instance['show_copyright']) ? 1 : 0;
+		$instance['show_telephone'] = !empty($new_instance['show_telephone']) ? 1 : 0;	
+		$instance['show_address'] = !empty($new_instance['show_address']) ? 1 : 0;	
+		$instance['center'] = !empty($new_instance['center']) ? 1 : 0;;
+		$instance['show_return'] = !empty($new_instance['show_return']) ? 1 : 0;
+		return $instance;
+	}
+
+	function form( $instance ) {
+		$instance = wp_parse_args( (array) $instance, 
+			array( 'show_return' => true,'nav_menu' => 0,'show_copyright' => true,'show_telephone' => true,'show_address' => true,'center' => true ) );
+		$nav_menu = isset( $instance['nav_menu'] ) ? (int) $instance['nav_menu'] : 0;
+		$show_return = isset( $instance['show_return'] ) ?  (bool) $instance['show_return'] : false;
+		$show_copyright = isset( $instance['show_copyright'] ) ? (bool) $instance['show_copyright'] : false;
+		$show_telephone = isset( $instance['show_telephone'] ) ? (bool) $instance['show_telephone'] : false;
+		$show_address = isset( $instance['show_address'] ) ? (bool) $instance['show_address'] : false;
+		$center = isset( $instance['center'] ) ? (bool) $instance['center'] : false;
+
+		$menus = get_terms( 'nav_menu', array( 'hide_empty' => false ) );
+		if ( !$menus ) {
+			echo '<p>'. sprintf( __('No menus have been created yet. <a href="%s">Create some</a>.', self::DOMAIN ), admin_url('nav-menus.php') ) .'</p>';
+			return;
+		}
+		?>
+		<p>
+			<label for="<?php echo $this->get_field_id('nav_menu'); ?>"><?php _e('Select Footer Menu:', self::DOMAIN ); ?></label>
+			<select id="<?php echo $this->get_field_id('nav_menu'); ?>" name="<?php echo $this->get_field_name('nav_menu'); ?>">
+		<?php 
+			$selected = empty($nav_menu) ? ' selected="selected"' : '';
+			echo ('<option'.$selected.' value="0">Do not show a menu</option>');
+			foreach ( $menus as $menu ) {
+				$selected = $nav_menu == $menu->term_id ? ' selected="selected"' : '';
+				echo '<option'. $selected .' value="'. $menu->term_id .'">'. $menu->name .'</option>';
+			}
+		?>
+			</select>
+		</p>
+		<input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id('show_copyright', self::DOMAIN ); ?>" name="<?php echo $this->get_field_name('show_copyright'); ?>"<?php checked( $show_copyright ); ?> />
+		<label for="<?php echo $this->get_field_id('show_copyright'); ?>"><?php _e( 'Show Copyright' ); ?></label><br />
+		<input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id('show_telephone', self::DOMAIN ); ?>" name="<?php echo $this->get_field_name('show_telephone'); ?>"<?php checked( $show_telephone ); ?> />
+		<label for="<?php echo $this->get_field_id('show_telephone'); ?>"><?php _e( 'Show Telephone number', self::DOMAIN  ); ?></label><br />
+		<input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id('show_address', self::DOMAIN ); ?>" name="<?php echo $this->get_field_name('show_address'); ?>"<?php checked( $show_address ); ?> />
+		<label for="<?php echo $this->get_field_id('show_address'); ?>"><?php _e( 'Show Address', self::DOMAIN  ); ?></label><br />
+		<input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id('center', self::DOMAIN ); ?>" name="<?php echo $this->get_field_name('center'); ?>"<?php checked( $center ); ?> />
+		<label for="<?php echo $this->get_field_id('center'); ?>"><?php _e( 'Center Menu',self::DOMAIN ); ?></label><br/>
+		<input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id('show_return', self::DOMAIN ); ?>" name="<?php echo $this->get_field_name('show_return'); ?>"<?php checked( $show_return ); ?> />
+		<label for="<?php echo $this->get_field_id('show_return'); ?>"><?php _e( 'Show Return To Top Link' ); ?></label><br />
+<?php
+	}
+}
+
+class Footer_Putter_TradeMark_Widget extends WP_Widget {
+	const DOMAIN = 'FooterCredits';
+		
+	function __construct() {
+		$widget_ops = array('description' => __( "Trademarks, Service Marks and Kitemarks" ) );
+		parent::__construct('footer_trademarks', __('TradeMarks Widget'), $widget_ops);
+	}
+
+	function widget( $args, $instance ) {
+		extract($args, EXTR_SKIP);
+
+		$category = isset($instance['category']) ? $instance['category'] : false;
+		$orderby = isset( $instance['orderby'] ) ? $instance['orderby'] : 'name';
+		$order = $orderby == 'rating' ? 'DESC' : 'ASC';
+		$limit = isset( $instance['limit'] ) ? $instance['limit'] : -1;
+
+		$links = wp_list_bookmarks(apply_filters('widget_links_args', array(
+			'echo' => 0, 
+			'title_before' => $before_title, 'title_after' => $after_title,
+			'title_li' => '', 'categorize' => false, 
+			'before' => '', 'after' => '',			
+			'category_before' => '', 'category_after' => '',
+			'show_images' => true, 'show_description' => false,
+			'show_name' => false, 'show_rating' => false,
+			'category' => $category, 'class' => 'trademark widget',
+			'orderby' => $orderby, 'order' => $order,
+			'limit' => $limit,
+		)));
+		echo $before_widget;
+		echo call_user_func(array(FOOTER_CREDITS,'filter_links'),$links);
+		echo $after_widget;
+	}
+
+	function update( $new_instance, $old_instance ) {
+		$new_instance = (array) $new_instance;
+
+		$instance['orderby'] = 'name';
+		if ( in_array( $new_instance['orderby'], array( 'name', 'rating', 'id', 'rand' ) ) )
+			$instance['orderby'] = $new_instance['orderby'];
+
+		$instance['category'] = intval( $new_instance['category'] );
+		$instance['limit'] = ! empty( $new_instance['limit'] ) ? intval( $new_instance['limit'] ) : -1;
+
+		return $instance;
+	}
+
+	function form( $instance ) {
+		$instance = wp_parse_args( (array) $instance, array( 'category' => false, 'orderby' => 'name', 'limit' => -1 ) );
+		$link_cats = get_terms( 'link_category' );
+		if ( ! $limit = intval( $instance['limit'] ) ) { $limit = -1; } ?>
+		<p>
+		<label for="<?php echo $this->get_field_id('category'); ?>"><?php _e( 'Select Link Category for Your Trademarks:', self::DOMAIN ); ?></label>
+		<select class="widefat" id="<?php echo $this->get_field_id('category'); ?>" name="<?php echo $this->get_field_name('category'); ?>">
+		<?php
+		foreach ( $link_cats as $link_cat ) {
+			echo '<option value="' . intval($link_cat->term_id) . '"'
+				. ( $link_cat->term_id == $instance['category'] ? ' selected="selected"' : '' )
+				. '>' . $link_cat->name . "</option>\n";
+		}
+		?>
+		</select>
+		<label for="<?php echo $this->get_field_id('orderby'); ?>"><?php _e( 'Sort by:', self::DOMAIN ); ?></label>
+		<select name="<?php echo $this->get_field_name('orderby'); ?>" id="<?php echo $this->get_field_id('orderby'); ?>" class="widefat">
+			<option value="name"<?php selected( $instance['orderby'], 'name' ); ?>><?php _e( 'Link title', self::DOMAIN); ?></option>
+			<option value="rating"<?php selected( $instance['orderby'], 'rating' ); ?>><?php _e( 'Link rating', self::DOMAIN ); ?></option>
+			<option value="id"<?php selected( $instance['orderby'], 'id' ); ?>><?php _e( 'Link ID', self::DOMAIN ); ?></option>
+			<option value="rand"<?php selected( $instance['orderby'], 'rand' ); ?>><?php _e( 'Random', self::DOMAIN ); ?></option>
+		</select>
+		</p>
+		<p>
+		<label for="<?php echo $this->get_field_id('limit'); ?>"><?php _e( 'Number of links to show:', self::DOMAIN ); ?></label>
+		<input id="<?php echo $this->get_field_id('limit'); ?>" name="<?php echo $this->get_field_name('limit'); ?>" type="text" value="<?php echo $limit == -1 ? '' : intval( $limit ); ?>" size="3" />
+		</p>
+<?php
+	}
+}
