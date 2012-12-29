f7e54fbbe402060c8a925aeb1e396fb30edf2706
powerblogservice <powerblogservice@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-29 11:53:50 +0000

    Tagging Footer Putter Version 1.2
    git-svn-id: https://plugins.svn.wordpress.org/footer-putter/tags/1.2@645795 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- main.php
+++ main.php
@@ -3,13 +3,13 @@
  * Plugin Name: Footer Putter
  * Plugin URI: http://www.diywebmastery.com/plugins/footer-putter/
  * Description: Put a footer on your site that boosts your credibility with both search engines and human visitors.
- * Version: 1.1
+ * Version: 1.2
  * Author: Russell Jamieson
  * Author URI: http://www.diywebmastery.com/about/
  * License: GPLv2+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  */
-define('FOOTER_PUTTER_VERSION','1.1');
+define('FOOTER_PUTTER_VERSION','1.2');
 define('FOOTER_PUTTER_FRIENDLY_NAME', 'Footer Putter') ;
 define('FOOTER_PUTTER_PLUGIN_NAME', 'footer-putter') ;
 define('FOOTER_PUTTER_HOME_URL','http://www.diywebmastery.com/plugins/footer-putter/');
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: http://www.diywebmastery.com/donate/
 Tags: footer, copyright, rel=nofollow
 Requires at least: 3.0
 Tested up to: 3.5
-Stable tag: 1.1
+Stable tag: 1.2
 License: GPLv2+
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -37,13 +37,14 @@ Put a footer on your site that boosts your credibility with both search engines 
 
 == Changelog ==
 
+= 1.2 = Fix display of telephone number
 = 1.1 = Fix link in admin.php from screenshot1.jpg to screenshot-1.jpg
 = 1.0 = Initial release
 
 == Upgrade Notice ==
 
-= 1.1 = 
-* Mandatory - fix link to screenshot-1.jpg
+= 1.2 = 
+* Mandatory - fix display of telephone number in footer
 
 == Links ==
 
