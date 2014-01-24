149b35b9cce3512041f16f43cb16c96883c0dc2f
powerblogservice <powerblogservice@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-24 13:52:34 +0000

    Tagging Footer Putter 1.7.1 - fix bug when loading under AJAX
    git-svn-id: https://plugins.svn.wordpress.org/footer-putter/tags/1.7.1@844534 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- main.php
+++ main.php
@@ -3,16 +3,15 @@
  * Plugin Name: Footer Putter
  * Plugin URI: http://www.diywebmastery.com/plugins/footer-putter/
  * Description: Put a footer on your site that boosts your credibility with both search engines and human visitors.
- * Version: 1.7
+ * Version: 1.7.1
  * Author: Russell Jamieson
  * Author URI: http://www.diywebmastery.com/about/
  * License: GPLv2+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  */
-define('FOOTER_PUTTER_VERSION','1.7');
+define('FOOTER_PUTTER_VERSION','1.7.1');
 define('FOOTER_PUTTER_FRIENDLY_NAME', 'Footer Putter') ;
 define('FOOTER_PUTTER_PLUGIN_NAME', plugin_basename(dirname(__FILE__))) ;
 define('FOOTER_PUTTER_HOME_URL','http://www.diywebmastery.com/plugins/footer-putter/');
 require_once(dirname(__FILE__) . '/classes/plugin.php');
-add_action ('init',  array('FooterCreditsPlugin', 'init'),0);
 ?>
